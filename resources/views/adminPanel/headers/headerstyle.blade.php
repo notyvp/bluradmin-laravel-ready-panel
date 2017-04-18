60567e9b9f50a1e0854b51426dddff571160de88
MehmetAli Saraç <msarac@MehmetAlis-MacBook-Pro.local>
2017-04-18 17:39:25 +0300

    initial commit

---
+++ resources/views/adminPanel/headers/headerstyle.blade.php
@@ -0,0 +1,8 @@
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900&subset=latin,greek,greek-ext,vietnamese,cyrillic-ext,latin-ext,cyrillic' rel='stylesheet' type='text/css'>
+
+<link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin/assets/img/favicon-16x16.png')}}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{asset('admin/assets/img/favicon-32x32.png')}}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{asset('admin/assets/img/favicon-96x96.png')}}">
+
+<link rel="stylesheet" href="{{asset('admin/styles/vendor-2cae343ef1.css')}}">
+<link rel="stylesheet" href="{{asset('admin/styles/auth-a200a050c1.css')}}">
